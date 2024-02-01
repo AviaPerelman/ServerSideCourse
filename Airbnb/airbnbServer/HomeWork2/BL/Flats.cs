31bfbc55e3307c7cd8247fc06f3b0eba6d51b462
Hindi <97826886+HinditProjects@users.noreply.github.com>
2024-02-01 23:43:12 +0200

    Logic Test on Insert Func

--- Airbnb/airbnbServer/HomeWork2/BL/Flats.cs
+++ Airbnb/airbnbServer/HomeWork2/BL/Flats.cs
@@ -26,17 +26,18 @@
         public int Id { get => id; set => id = value; }
         public string City { get => city; set => city = value; }
         public string Address { get => address; set => address = value; }
+        public double Price { get => price; set => price = value; }
         public int Numbers_of_rooms { get => numbers_of_rooms; set => numbers_of_rooms = value; }
-        public double Price
-        {
-            get => price;
-
-            set { price = Discount(value); }
-        }
-
 
         public bool Insert()
         {
+            //Logic Check ->
+
+            if (numbers_of_rooms > 1 && price > 100)
+            {
+                price *= 0.9;
+            }
+            
             foreach (var item in FlatsList)
             {
                 if (item.id == this.id)
@@ -51,17 +52,7 @@
 
         public List<Flat> Read() => FlatsList;
 
-        public double Discount(double price)
-        {
-            if (numbers_of_rooms > 1 && price > 100)
-            {
-                price *= 0.9;
-            }
-
-            return price;
-
-        }
-
+       
         public List<Flat> ReadByPriceAndCity(string city, double maxPrice)
         {
             List<Flat> selectedList = new List<Flat>();
