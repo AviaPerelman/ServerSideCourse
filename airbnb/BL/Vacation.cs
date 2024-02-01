f0f6797ed7e5b548ffcee5644e27db8540f2dcc5
AviaPerelman <154759994+AviaPerelman@users.noreply.github.com>
2024-02-01 15:55:50 +0200

    upload server

---
+++ airbnb/BL/Vacation.cs
@@ -0,0 +1,115 @@
+﻿namespace HomeWork2.BL
+{
+    public class Vacation
+    {
+        int id;
+        int userId;
+        int flatId;
+        DateTime startDate;
+        DateTime endDate;
+
+
+        static List<Vacation> vacationsList = new List<Vacation>();
+
+        public Vacation() { }
+        public Vacation(int id, int userId, int flatId, DateTime startDate, DateTime endDate)
+        {
+
+            Id = id;
+            UserId = userId;
+            FlatId = flatId;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public int UserId { get => userId; set => userId = value; }
+        public int FlatId
+        {
+            get => flatId;
+            set
+            {
+                flatId = CheckFlat(value);
+
+            }
+        }
+        public DateTime StartDate { get => startDate; set => startDate = value; }
+        public DateTime EndDate
+        {
+            get => endDate;
+            set
+            {
+                if (value > StartDate)
+                {
+                    endDate = value;
+                }
+                else endDate = DateTime.MinValue;
+
+            }
+
+        }
+
+        public int CheckFlat(int value)
+        {
+            List<Flat> CheckFlats = Flat.FlatsList;
+            foreach (Flat item in CheckFlats)
+            {
+                if (item.Id == value)
+                {
+                    return value;
+                }
+            }
+            return -1;
+        }
+
+        public bool Insert()
+        {
+            if (this.flatId == -1 || this.endDate == DateTime.MinValue || IsRented(this))
+            {
+                return false;
+            }
+
+            foreach (var item in vacationsList)
+            {
+                if (item.id == this.id)
+                {
+                    return false;
+                }
+            }
+
+            vacationsList.Add(this);
+            return true;
+        }
+
+        public bool IsRented(Vacation obj)
+        {
+            foreach (var item in vacationsList)
+            {
+                if (item.flatId == obj.flatId && !(item.endDate < obj.startDate || item.startDate > obj.endDate))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public List<Vacation> Read()
+        {
+            return vacationsList;
+        }
+
+        public List<Vacation> ReadByDates(DateTime from, DateTime to)
+        {
+            List<Vacation> selectedItems = new List<Vacation>();
+            foreach (var item in vacationsList)
+            {
+                if (item.startDate >= from && item.endDate <= to)
+                {
+                    selectedItems.Add(item);
+                }
+            }
+            return selectedItems;
+        }
+    }
+}
+
